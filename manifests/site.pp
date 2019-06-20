28095177b5d04b736926e184c82ea522873b679a
enemorio <enemorio@gmail.com>
2019-06-20 16:54:41 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,13 @@
 node default {
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
   file {'/root/README':
     ensure => file,
     content => 'Testerino :)
 ',
     owner => 'root',
   }
-}
 
-node 'master.puppet.vm' {
-  include role::master_server
 }
